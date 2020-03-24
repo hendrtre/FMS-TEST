a963778b9732a06606414540838caf7563503f6b
Trent Hendrickson <trentdev2020@gmail.com>
2020-03-24 15:36:47 -0600

    added 'flask_cors' in order to work with multipul localhosts

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_heroku import Heroku 
+from flask_cors import CORS
 import os 
 
 
 app = Flask(__name__)
+CORS(app) 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
